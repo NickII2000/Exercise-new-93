73d49016c05334eeea544ce96b618bb992311b55
Nick <Nick_II2000@mail.ru>
2022-07-25 09:47:04 +0600

    21-th commit

--- js/script.js
+++ js/script.js
@@ -422,37 +422,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
     });
 
-    // showSlides(slideIndex);
-
-    // total.textContent = slides.length < 10 ? `0${slides.length}` : slides.length;
-
-    // function showSlides(n) {
-    //     if (n > slides.length) {
-    //         slideIndex = 1;
-    //     }
-
-    //     if (n < 1) {
-    //         slideIndex = slides.length;
-    //     }
-
-    //     slides.forEach(item => item.style.display = 'none');
-
-    //     slides[slideIndex - 1].style.display = 'block';
-
-    //     current.textContent = slides.length < 10 ? `0${slideIndex}` : slideIndex;
-
-    // }
-
-    // function plusSlides(n) {
-    //     showSlides(slideIndex += n);
-    // }
-
-    // prev.addEventListener('click', () => {
-    //     plusSlides(-1);
-    // });
-
-    // next.addEventListener('click', () => {
-    //     plusSlides(1);
-    // });
-
 });
